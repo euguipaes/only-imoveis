300a329f1d7f28ac0b13b6989e7abe6e43392666
Guilherme Paes <127236494+euguipaes@users.noreply.github.com>
2023-05-24 15:40:26 -0300

    Delete routes directory

--- routes/imoveis.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getImoveis } from "../controllers/imoveis.js";
-
-const router = express.Router();
-
-router.get("/imoveis", getImoveis);
-
-export default router;
